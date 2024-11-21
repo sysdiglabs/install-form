e1098936157b743db980a4346fbd2d9be9b31fbd
sleevasrujana <sleeva.pentareddy@sysdig.com>
2024-11-21 09:03:54 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -389,9 +389,9 @@ function setHelmCommandAgentConfigs(params) {
   } 
 
   if (params.registryCheckbox.checked) {
-    helmCommandAgentConfigs += "<br>&nbsp;&nbsp; --set agent.slim.image.tag=" + params.registryInputs[4].value + " \\";
+    helmCommandAgentConfigs += "<br>&nbsp;&nbsp; --set agent.image.tag=" + params.registryInputs[4].value + " \\";
     helmCommandAgentConfigs += "<br>&nbsp;&nbsp; --set global.image.registry=" + params.registryInputs[0].value + " \\";
-    helmCommandAgentConfigs += "<br>&nbsp;&nbsp; --set agent.slim.image.repository=" + params.registryInputs[2].value + " \\";
+    helmCommandAgentConfigs += "<br>&nbsp;&nbsp; --set agent.image.repository=" + params.registryInputs[2].value + " \\";
 //     helmCommandAgentConfigs += "<br>&nbsp;&nbsp; --set clusterShield.image.registry=" + params.registryInputs[0].value + " \\";
     helmCommandAgentConfigs += "<br>&nbsp;&nbsp; --set clusterShield.image.repository=" + params.registryInputs[3].value + " \\";
     helmCommandAgentConfigs += "<br>&nbsp;&nbsp; --set clusterShield.image.tag=" + params.registryInputs[5].value + " \\";
@@ -401,7 +401,7 @@ function setHelmCommandAgentConfigs(params) {
     }
   }
   else {
-    helmCommandAgentConfigs += "<br>&nbsp;&nbsp; --set agent.slim.image.tag=" + params.agentTagsSelect.value + " \\";
+    helmCommandAgentConfigs += "<br>&nbsp;&nbsp; --set agent.image.tag=" + params.agentTagsSelect.value + " \\";
     helmCommandAgentConfigs += "<br>&nbsp;&nbsp; --set clusterShield.image.tag=" + params.runtimeScannerTagsSelect.value + " \\";
   }
 
